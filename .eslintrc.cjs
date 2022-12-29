dbdc9601b9ec82632206e6e94393f59929812005
Sathish K <sathishkcontact@gmail.com>
2022-12-29 16:47:44 +0530

    feat(inital setup): added all linting rules
    enabled es module , changed jade to pug

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier", "eslint:recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "global-require": "off",
+    "no-use-before-define": [
+      "error",
+      {
+        functions: true,
+        classes: true,
+        variables: true,
+      },
+    ],
+    "no-unused-vars": "error",
+    "no-console": "warn",
+    "func-names": "error",
+    "no-plusplus": "error",
+    "no-process-exit": "error",
+    "class-methods-use-this": "error",
+    "import/no-named-as-default": "off",
+    "import/extensions": [0, { js: "always" }],
+  },
+};
